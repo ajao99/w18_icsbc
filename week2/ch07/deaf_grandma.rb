2d7a696a3de62e43a721d773234b3993df6ca070
ajao99 <moving.forward.99@berkeley.edu>
2019-01-02 19:41:28 -0800

    assignment complete

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -1,7 +1,14 @@
 puts 'COME GIVE GRANNY A KISS!'
 
-while () # Fill in the while condition
-  ### Your Code Here ###
+while (true)
+  input = gets.chomp
+  if input == input.downcase || input == input.capitalize
+  puts 'HUH?! SPEAK UP SONNY!'
+  elsif input == 'BYE'
+    break
+  elsif input == input.upcase
+    puts 'NO, NOT SINCE ' + (rand(21) + 1930).to_s + '!'
+  end
 end
 
 puts 'OK SONNY, TALK AGAIN SOON'
